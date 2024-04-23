38dfdba007cca0475678bb6fc637e8fb484960e3
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-04-23 20:31:10 +0300

    props basics lab

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{props.bio}</p>
+      <p>{!props.bio?null:props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links linkedin = {props.linkedin} github = {props.github} />
     </div>
