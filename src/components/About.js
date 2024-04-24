36a5222bafb369413cb3994398832ead7260aca8
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-04-24 18:37:19 +0300

    adds conditional render for prop

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{!props.bio?null:props.bio}</p>
+      {props.bio?<p>{props.bio}</p>:null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links linkedin = {props.linkedin} github = {props.github} />
     </div>
